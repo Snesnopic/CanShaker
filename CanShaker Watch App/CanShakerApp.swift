b5f7b02d4a507b49adb4e3464cd8c56d0ad28ce7
Snesnopic <snesnopic@gmail.com>
2024-02-16 16:31:01 +0100

    Nasty if to remove the tabbar indicator

--- CanShaker Watch App/CanShakerApp.swift
+++ CanShaker Watch App/CanShakerApp.swift
@@ -13,16 +13,30 @@ struct CanShaker_Watch_AppApp: App {
     @State var currentState:SessionState = .start
     var body: some Scene {
         WindowGroup {
-            TabView(selection: $firstTabView,
-                    content:  {
-                SessionView(currentState: $currentState,firstTabView: $firstTabView)
-                    .tabItem { Label(String(localized: "Session"), systemImage: "figure") }
-                LastStatsView()
-                    .tabItem { Label(String(localized: "Last session"), systemImage: "chart.bar.xaxis") }
-                BadgesView()
-                    .tabItem { Label(String(localized: "Badges"), systemImage: "trophy.fill") }
-            })
-            .tabViewStyle(.carousel)            
+            if currentState == .shaking {
+                TabView(selection: $firstTabView,
+                        content:  {
+                    SessionView(currentState: $currentState,firstTabView: $firstTabView)
+                        .tabItem { Label("Session", systemImage: "figure") }
+                    LastStatsView()
+                        .tabItem { Label("Last session", systemImage: "chart.bar.xaxis") }
+                    BadgesView()
+                        .tabItem { Label("Badges", systemImage: "trophy.fill") }
+                })
+                .tabViewStyle(.page(indexDisplayMode: .never))
+            }
+            else {
+                TabView(selection: $firstTabView,
+                        content:  {
+                    SessionView(currentState: $currentState,firstTabView: $firstTabView)
+                        .tabItem { Label("Session", systemImage: "figure") }
+                    LastStatsView()
+                        .tabItem { Label("Last session", systemImage: "chart.bar.xaxis") }
+                    BadgesView()
+                        .tabItem { Label("Badges", systemImage: "trophy.fill") }
+                })
+                .tabViewStyle(.verticalPage)
+            }
         }
     }
 }
