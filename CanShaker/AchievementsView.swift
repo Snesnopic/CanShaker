0eb220c47e142f8b614610572bc48201ebfd37b8
Snesnopic <snesnopic@gmail.com>
2024-02-16 15:31:19 +0100

    First try for achievements view

--- CanShaker/AchievementsView.swift
+++ CanShaker/AchievementsView.swift
@@ -10,8 +10,22 @@ import SwiftUI
 struct AchievementsView: View {
     var body: some View {
         NavigationStack {
-            Text("Hello, World!")
+            List {
+                NavigationLink {
+                    
+                } label: {
+                    HStack {
+                        VStack {
+                            Text("Serial shaker!").bold()
+                            Text("5 times a day!").foregroundStyle(.secondary)
+                        }
+                        Spacer()
+                        Text("Detail").foregroundStyle(.secondary)
+                    }
+                }
 
+            }
+            .listStyle(.grouped)
             .navigationTitle("Achievements")
         }
     }
