2d758a52490e8b6f4dde69d3782201bd7f0770ba
Snesnopic <snesnopic@gmail.com>
2024-02-16 15:30:58 +0100

    First try for session view

---
+++ CanShaker Watch App/SessionView.swift
@@ -0,0 +1,52 @@
+//
+//  SessionView.swift
+//  CanShaker Watch App
+//
+//  Created by Giuseppe Francione on 15/02/24.
+//
+
+import SwiftUI
+import HealthKit
+enum SessionState {
+    case start
+    case shaking
+    case done
+}
+struct SessionView: View {
+    @Binding var currentState: SessionState
+    @Binding var firstTabView:Int
+    @State var secondTabView:Int = 2
+    var body: some View {
+        NavigationStack {
+            switch currentState {
+            case .start:
+                Button(action: {
+                    currentState = .shaking
+                }, label: {
+                    ZStack {
+                        Circle()
+                            .foregroundStyle(.orange)
+                        Text("Start session")
+                            .bold()
+                            .foregroundStyle(.background)
+                    }
+                })
+                .buttonStyle(.plain)
+            case .shaking:
+                TabView(selection: $secondTabView,
+                        content:  {
+                    Button(role: .destructive) {
+                        firstTabView = 2
+                    } label: {
+                        Text("I'm done")
+                    }.tag(1)
+                    Text("Shaking").tag(2)
+                })
+                .tabViewStyle(.page)
+            case .done:
+                Text("Done2!")
+            }
+            
+        }
+    }
+}
