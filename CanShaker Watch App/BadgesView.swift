0c0beb1fb8903cc8d30efacf7e9aea27205ee1c6
Snesnopic <snesnopic@gmail.com>
2024-02-19 17:01:33 +0100

    Added Hero animation

--- CanShaker Watch App/BadgesView.swift
+++ CanShaker Watch App/BadgesView.swift
@@ -8,23 +8,36 @@
 import SwiftUI
 
 struct BadgesView: View {
+    @Namespace private var animation
+    @State var isPresented:Bool = false
     var body: some View {
         NavigationStack {
-            Grid {
-                ForEach(1...3, id: \.self){ Int in
-                    GridRow {
-                        ForEach(1...3, id: \.self) {_ in 
-                            Circle().foregroundStyle(Color(
-                                red: .random(in: 0...1),
-                                green: .random(in: 0...1),
-                                blue: .random(in: 0...1)))
-                        }.padding()
+            if !isPresented {
+                Grid {
+                    ForEach(1...3, id: \.self){ i in
+                        GridRow {
+                            ForEach(1...3, id: \.self) {j in
+                                Button(action: {
+                                    withAnimation {
+                                        isPresented = true
+                                    }
+                                }, label: {
+                                    Circle()
+                                        .matchedGeometryEffect(id: "circle\(i)\(j)", in: animation)
+                                        .animation(.easeInOut)
+                                        .foregroundStyle(.red)
+                                    
+                                })
+                                .buttonStyle(.plain)
+                            }
+                        }
                     }
                 }
+                .navigationTitle("Badges")
             }
-            
-           
-            .navigationTitle("Badges")
+            else {
+                BadgeDetailView(animation: animation, isPresented: $isPresented, badge: Badge(name: "Prova", color: Color.red, description: "Prova test prova ipsum color prova testum"))
+            }
         }
     }
 }
