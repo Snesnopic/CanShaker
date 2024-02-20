13c7b3627f49c415f8ed413a2d9f3cd527cac0af
Snesnopic <snesnopic@gmail.com>
2024-02-20 10:51:54 +0100

    View to be fixed (is not a grid)

--- CanShaker Watch App/BadgesView.swift
+++ CanShaker Watch App/BadgesView.swift
@@ -13,27 +13,26 @@ struct BadgesView: View {
     var body: some View {
         NavigationStack {
             if !isPresented {
-                Grid {
-                    ForEach(1...3, id: \.self){ i in
-                        GridRow {
-                            ForEach(1...3, id: \.self) {j in
-                                Button(action: {
-                                    withAnimation {
-                                        isPresented = true
-                                    }
-                                }, label: {
-                                    Circle()
-                                        .matchedGeometryEffect(id: "circle\(i)\(j)", in: animation)
-                                        .animation(.easeInOut)
-                                        .foregroundStyle(.red)
-                                    
-                                })
-                                .buttonStyle(.plain)
-                            }
+                ScrollView {
+                    GridLayout() {
+                        ForEach(allBadges, id: \.id) { badge in
+                            Button(action: {
+                                withAnimation {
+                                    isPresented = true
+                                }
+                            }, label: {
+                                Circle()
+                                    .frame(width: 20)
+                                    .matchedGeometryEffect(id: badge.id, in: animation)
+                                    .foregroundStyle(.red)
+                                
+                            })
+                            .buttonStyle(.plain)
                         }
                     }
+                    
+                    .navigationTitle("Badges")
                 }
-                .navigationTitle("Badges")
             }
             else {
                 BadgeDetailView(animation: animation, isPresented: $isPresented, badge: Badge(name: "Prova", color: Color.red, description: "Prova test prova ipsum color prova testum"))
