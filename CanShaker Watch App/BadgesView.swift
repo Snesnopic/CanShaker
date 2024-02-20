360207e7a22bfea32745505f55bcbdc3be924e8b
Snesnopic <snesnopic@gmail.com>
2024-02-20 14:44:26 +0100

    Almost fully fixed code, still a bit messy

--- CanShaker Watch App/BadgesView.swift
+++ CanShaker Watch App/BadgesView.swift
@@ -10,32 +10,57 @@ import SwiftUI
 struct BadgesView: View {
     @Namespace private var animation
     @State var isPresented:Bool = false
+    @State var selectedBadge:Badge = Badge(name: "", color: Color.white, description: "")
     var body: some View {
         NavigationStack {
             if !isPresented {
                 ScrollView {
-                    GridLayout() {
-                        ForEach(allBadges, id: \.id) { badge in
-                            Button(action: {
-                                withAnimation {
-                                    isPresented = true
-                                }
-                            }, label: {
-                                Circle()
-                                    .frame(width: 20)
-                                    .matchedGeometryEffect(id: badge.id, in: animation)
-                                    .foregroundStyle(.red)
-                                
-                            })
-                            .buttonStyle(.plain)
+                    Grid() {
+                        ForEach((1...allBadges.count/3), id: \.self){
+                            i in
+                            GridRow {
+                                Button(action: {
+                                    selectedBadge = allBadges[(i-1)*3]
+                                    withAnimation {
+                                        isPresented = true
+                                    }
+                                }, label: {
+                                    ExtractedView(badge: allBadges[(i-1)*3], animation: animation)
+                                })
+                                .buttonStyle(.plain)
+                                Button(action: {
+                                    selectedBadge = allBadges[(i-1)*3 + 1]
+                                    withAnimation {
+                                        isPresented = true
+                                    }
+                                }, label: {
+                                    ExtractedView(badge: allBadges[(i-1)*3 + 1],animation: animation)
+                                    
+                                })
+                                .buttonStyle(.plain)
+                                Button(action: {
+                                    selectedBadge = allBadges[(i-1)*3 + 2]
+                                    withAnimation {
+                                        isPresented = true
+                                    }
+                                }, label: {
+                                    ExtractedView(badge: allBadges[(i-1)*3 + 2],animation: animation)
+                                    
+                                })
+                                .buttonStyle(.plain)
+                            }
                         }
                     }
+                    .onAppear(perform: {
+                        print("Allbadges count: \(allBadges.count)")
+                        print("Allbadges count/3: \(allBadges.count/3)")
+                    })
                     
                     .navigationTitle("Badges")
                 }
             }
             else {
-                BadgeDetailView(animation: animation, isPresented: $isPresented, badge: Badge(name: "Prova", color: Color.red, description: "Prova test prova ipsum color prova testum"))
+                BadgeDetailView(animation: animation, isPresented: $isPresented, badge: selectedBadge)
             }
         }
     }
@@ -43,4 +68,15 @@ struct BadgesView: View {
 
 #Preview {
     BadgesView()
+}
+
+struct ExtractedView: View {
+    var badge:Badge
+    var animation:Namespace.ID
+    var body: some View {
+        Circle()
+            .frame(width: 50)
+            .matchedGeometryEffect(id: badge.id, in: animation)
+            .foregroundStyle(badge.color)
+    }
 }
