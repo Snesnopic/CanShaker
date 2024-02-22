7bc356462f1f0bf060751098cca41f4850447971
Dave-Ed-Cast <dcastaldi23@fed.idserve.net>
2024-02-22 15:57:09 +0100

    Achievement view has been defined, defined the mechanism for evaluating completion and recently achieved mechanisms (also considered the first usage of app)

--- CanShaker/Model/Achievement.swift
+++ CanShaker/Model/Achievement.swift
@@ -6,30 +6,49 @@
 //
 
 import Foundation
+import SwiftUI
+
 
 class Achievement: Identifiable{
+ 
+    
     let id: UUID
     let badge: String
     let title: String
     let subTitle: String
     let description: String
+    let completion: Int
     var isAchieved: Bool
     
-    init(id: UUID, badge: String, title: String, subTitle: String, description: String, isAchieved: Bool) {
+    init(id: UUID, badge: String, title: String, subTitle: String, description: String, completion: Int, isAchieved: Bool) {
         self.id = id
         self.badge = badge
         self.title = title
         self.subTitle = subTitle
         self.description = description
+        self.completion = completion
         self.isAchieved = isAchieved
     }
 }
 
 extension Achievement {
     static let allAchievements = [
-    Achievement(id: UUID(), badge: "", title: "Serial Shaker", subTitle: "5 times a day!", description: "You have shaked your can for 5 times a day!", isAchieved: false),
-    Achievement(id: UUID(), badge: "", title: "Top Speed", subTitle: "Shaking speed is over 130km/h!", description: "You have shaked your can really fast!", isAchieved: false),
-    Achievement(id: UUID(), badge: "", title: "Who goes slow stay healthy and goes far", subTitle: "Finish a session in over 1 hour", description: "Something something", isAchieved: false)
+        Achievement(id: UUID(), badge: "", title: "title1", subTitle: "subtitle1", description: "You have shaked your can for 5 times a day!", completion: 33, isAchieved: false),
+        Achievement(id: UUID(), badge: "", title: "title2", subTitle: "subtitle2", description: "You have shaked your can really fast!", completion: 22, isAchieved: false),
+        Achievement(id: UUID(), badge: "", title: "title3", subTitle: "subtitle3", description: "Something something", completion: 84, isAchieved: true)
     ]
     
+    static func sortAlmostCompleted() -> [Achievement] {
+        let sortedAchievements = allAchievements.sorted { $0.completion > $1.completion }
+        
+        let almostCompleted = sortedAchievements.prefix(3)
+        return Array(almostCompleted)
+    }
+    
+    static func sortLastAchieved() -> [Achievement] {
+        let sortedAchievements = allAchievements.sorted { $0.id.uuidString > $1.id.uuidString }
+        return sortedAchievements
+    }
 }
+
+
--- CanShaker/View/Achievement/AchievementsView.swift
+++ CanShaker/View/Achievement/AchievementsView.swift
@@ -8,56 +8,118 @@
 import SwiftUI
 
 struct AchievementsView: View {
+    
+    let achievedAchievements = Achievement.allAchievements.filter { $0.isAchieved }
+    
+    
+        
     var body: some View {
+        
         NavigationStack {
             Color(uiColor: UIColor.secondarySystemBackground).ignoresSafeArea().overlay {
                 
-                //TODO: redo all the design :'c
                 ScrollView {
-                    VStack{
-                        ForEach(Achievement.allAchievements){ achieve in
-                            
-                            NavigationLink(destination: AchievementDetailView(), label: {
-                                
-                                //TODO: every button should/could have the same size
-                                ZStack{
-                                    RoundedRectangle(cornerRadius: 15)
-                                        .foregroundStyle(.white)
-                                    HStack{
-                                        Image(systemName: "diamond")
-                                            .padding(.horizontal, 4)
-                                        
-                                        VStack{
-                                            Text(achieve.title)
-                                                .font(.title3)
-                                                
-                                            +
-                                            Text("\n" + achieve.subTitle)
-                                                .font(.subheadline)
-                                                .foregroundStyle(.gray)
-                                        }
-                                        Spacer()
-                                        Image(systemName: "chevron.right")
-                                            .foregroundStyle(.gray)
-                                            .padding(.horizontal, 10)
-                                    }
-                                    .foregroundStyle(.black)
-                                    .multilineTextAlignment(.leading)
-                                    .frame(width: 380, alignment: .leading)
-                                    
-                                    //TODO: change these frames width with (device_width - (3~5% device_width))
-                                }
-                                .frame(width: 380)
-                                
-                            })
-                            
-                        }
-                        
-                        
+                    VStack() {
+                        almostThere
+                            .offset(y: 40)
+                        recentlyCompleted
+                            .offset(y: 70)
+                        listOfAchievements
+                            .offset(y: 100)
                     }
                 }
             }
             .navigationTitle("Achievements")
+        }
+    }
+    
+    var almostThere: some View {
+        
+        //TODO: needs to show the 3 closer to completion
+        ZStack(alignment: .leading){
+            Text("Almost There!")
+                .font(.title2)
+                .fontWeight(.semibold)
+                .offset(y: -105)
+                .padding()
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundStyle(.white)
+                .padding()
+            HStack{
+                ForEach(Achievement.sortAlmostCompleted()) { achievement in
+                    VStack() {
+                        Circle()
+                            .frame(width: 80, height: 80)
+                        Text(achievement.title)
+                            .font(.title3)
+                        Text(achievement.subTitle)
+                            .font(.subheadline)
+                    }
+                    .padding()
+                }
+            }
+            .padding()
+        }
+    }
+    
+    var recentlyCompleted: some View {
+        
+        ZStack(alignment: .leading) {
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundStyle(.white)
+                .padding()
+            Text("Recently achieved")
+                .font(.title2)
+                .fontWeight(.semibold)
+                .offset(y: -105)
+                .padding()
+            //TODO: this is to prevent the first time the users uses the app to see achievements that he didn't acquire, needs to conform to only last 3 achieved
+            if(!achievedAchievements.isEmpty) {
+                HStack{
+                    ForEach(achievedAchievements){ achievement in
+                        VStack() {
+                            Circle()
+                                .frame(width: 80, height: 80)
+                            Text(achievement.title)
+                                .font(.title3)
+                            Text(achievement.subTitle)
+                                .font(.subheadline)
+                            
+                            
+                        }
+                        .padding()
+                    }
+                }
+                .padding()
+            }
+        }
+    }
+    
+    var listOfAchievements: some View {
+        
+        ZStack(alignment: .leading){
+            Text("More Achievements")
+                .font(.title2)
+                .fontWeight(.semibold)
+                .offset(y: -105)
+                .padding()
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundStyle(.white)
+                .padding()
+            HStack{
+                ForEach(Achievement.allAchievements){ achievement in
+                    VStack() {
+                        Circle()
+                            .frame(width: 80, height: 80)
+                        Text(achievement.title)
+                            .font(.title3)
+                        Text(achievement.subTitle)
+                            .font(.subheadline)
+                    }
+                    .padding()
+                }
+            }
+            .padding()
         }
     }
 }
