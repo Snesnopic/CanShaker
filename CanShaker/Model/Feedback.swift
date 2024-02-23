6be35c973bbecdd1f4760d449bbbcfdb59722e0d
Giuseppe Casillo <g.casillox2@gmail.com>
2024-02-23 10:28:36 +0100

    Added feedback Class

--- CanShaker/Model/Feedback.swift
+++ CanShaker/Model/Feedback.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+class Feedback: Identifiable {
+    let id: UUID
+    let sentence: String
+    let type: feedbackType
+    let category: feedbackCategory
+    let condition: String
+    
+    init(id: UUID, sentence: String, type: feedbackType, category: feedbackCategory, condition: String) {
+        self.id = id
+        self.sentence = sentence
+        self.type = type
+        self.category = category
+        self.condition = condition
+    }
+}
+
+
+enum feedbackType {
+    case insult
+    case compliment
+}
+
+enum feedbackCategory {
+    case speed
+    case heartBeat
+    case calories
+}
