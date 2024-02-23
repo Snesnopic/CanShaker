8ffe9bb4b8056977e71d4a4ab3c81e73c9ab6b95
Snesnopic <snesnopic@gmail.com>
2024-02-23 20:52:25 +0100

    Axis Labels fixed, also now accesses the session from the database

--- CanShaker Watch App/View/LastStatsView.swift
+++ CanShaker Watch App/View/LastStatsView.swift
@@ -8,33 +8,55 @@
 import SwiftUI
 import Charts
 import CoreMotion
+import SwiftData
 
 struct LastStatsView: View {
-    @ObservedObject var motionDataManager = MotionDataManager.shared
+    @Query var sessions:[Session]
     var body: some View {
         NavigationStack {
-            Chart {
-                ForEach(motionDataManager.accelData.keys.sorted(),id: \.self) {
-                    time in
-                    BarMark (x: .value("Time",time),
-                             y: .value("Acceleration", motionDataManager.getTotalAcceleration(accel: motionDataManager.accelData[time]!)))
+            if !sessions.isEmpty {
+                Chart {
+                    ForEach(sessions.last!.accelData.keys.sorted(),id: \.self) {
+                        time in
+                        BarMark (x:
+    //                            .value("Time", time),
+                            .value("Time", Date(timeIntervalSince1970: time),unit: .second),
+                                 y: .value("Acceleration", sessions.last!.accelData[time]!.getTotalAcceleration()))
+                    }
                 }
+                .chartXAxis{
+                    AxisMarks(values: .automatic(desiredCount: 5)) {
+                        AxisValueLabel(format: Date.FormatStyle().minute(.defaultDigits).second(.defaultDigits)).offset(x: -5)
+                    }
+                }
+                .chartYAxis {
+                    AxisMarks(values: .automatic(desiredCount: 5))
+                }
+                .chartXAxisLabel("Time")
+    //            .chartYAxisLabel("Speed")
+    //            .chartLegend(content: {
+    //                Text("Acceleration")
+    //            })
+                .chartForegroundStyleScale([
+                    "Test": .blue
+                ])
+                .navigationTitle("Last session")
+
+    
             }
-            .chartLegend(content: {
-                Text("Acceleration")
-            })
-            .chartForegroundStyleScale([
-                "Test": .blue
-            ])
-            .navigationTitle("Last session")
+            else {
+                Text("No sessions yet!")
+                    .navigationTitle("Last session")
+
+            }
         }
     }
 }
-
-#Preview {
-    for i in 0...10 {
-        MotionDataManager.shared.accelData[Double(i)] = CMAcceleration(x: Double.random(in: 0...3), y: Double.random(in: 0...3), z: Double.random(in: 0...3))
-    }
-    
-   return LastStatsView(motionDataManager: MotionDataManager.shared)
-}
+//
+//#Preview {
+//    for i in 0...100 {
+//        MotionDataManager.shared.accelData[Double(i)] = CMAcceleration(x: Double.random(in: 0...3), y: Double.random(in: 0...3), z: Double.random(in: 0...3))
+//    }
+//    
+//    return LastStatsView(motionDataManager: MotionDataManager.shared)
+//}
