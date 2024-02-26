7de4641ccb2c9327d37468f4f14c582e139d95ce
Giuseppe Casillo <g.casillox2@gmail.com>
2024-02-26 17:43:50 +0100

    Cleaned LastSessionView (added specific files for graphs)

---
+++ CanShaker/View/Dashboard/Components/BpmGraphView.swift
@@ -0,0 +1,18 @@
+//
+//  BpmGraphView.swift
+//  CanShaker
+//
+//  Created by Giuseppe Casillo on 26/02/24.
+//
+
+import SwiftUI
+
+struct BpmGraphView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BpmGraphView()
+}
