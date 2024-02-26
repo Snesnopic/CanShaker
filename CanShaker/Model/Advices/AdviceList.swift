f857589b1b678b85b3bf18d89cc53ec2fb6cb2e7
Dave-Ed-Cast <dcastaldi23@fed.idserve.net>
2024-02-26 10:53:27 +0100

    Advices creation

--- CanShaker/Model/Advices/AdviceList.swift
+++ CanShaker/Model/Advices/AdviceList.swift
@@ -9,9 +9,8 @@ import Foundation
 
 extension Advice {
     static let list = [
-        Advice(title: "Placeholder", description: "Lorem ipsum tunz tunz tunz", category: .hygiene),
-        Advice(title: "Placeholder2", description: "Lorem ipsum tunz tunz tunz", category: .hygiene),
-        Advice(title: "Placeholde3", description: "Lorem ipsum tunz tunz tunz", category: .hygiene),
-        Advice(title: "Placeholde4", description: "Lorem ipsum tunz tunz tunz", category: .hygiene)
+        Advice(title: "Clean hands", description: "Wash hands before and after: Before engaging in masturbation, ensure that your hands are clean to minimize the introduction of bacteria or other contaminants to the genital area. Afterward, wash your hands thoroughly to remove any bodily fluids and reduce the risk of spreading bacteria to other parts of your body or objects you may touch.", category: .hygiene),
+        Advice(title: "Lube up for comfort", description: "Use lubrication: Using a water-based lubricant during masturbation can help reduce friction and irritation on the skin of the penis. This is especially important if you're prone to dryness or sensitivity. Avoid using products like lotions, oils, or creams that may contain ingredients that could irritate the delicate skin of the genital area.", category: .hygiene),
+        Advice(title: "Post session hygiene", description: "Clean genitals afterward: After masturbation, gently wash the genital area with warm water and a mild, unscented soap to remove any remaining lubricant, semen, or other bodily fluids. Be sure to rinse thoroughly and pat dry with a clean towel. Keeping the genital area clean helps prevent the buildup of bacteria and reduces the risk of skin irritation or infection.", category: .hygiene)
     ]
 }
