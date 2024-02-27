43b695a231198278f29729dcd5a892adfbe67185
Snesnopic <snesnopic@gmail.com>
2024-02-27 10:14:07 +0100

    Starter code to fetch health data (calories and heart beats)

--- CanShaker Watch App/Model/MotionDataManager.swift
+++ CanShaker Watch App/Model/MotionDataManager.swift
@@ -8,6 +8,7 @@
 import Foundation
 import CoreMotion
 import SwiftData
+import HealthKit
 
 class MotionDataManager: ObservableObject {
     static var shared = MotionDataManager()
@@ -15,13 +16,48 @@ class MotionDataManager: ObservableObject {
     let queue = OperationQueue()
     var accelData: [TimeInterval:CMAcceleration] = [:]
     var session:Session? = nil
+    let healthStore:HKHealthStore
+    var workoutSession: HKWorkoutSession?
+    var workoutBuilder: HKLiveWorkoutBuilder?
     private init() {
         motion.deviceMotionUpdateInterval = 1.0 / 5.0
         motion.showsDeviceMovementDisplay = true
+        
+        //HealthKit part
+        healthStore = HKHealthStore()
+        let typesToShare:Set = [HKQuantityType.workoutType()]
+        print("Types to share: \(typesToShare)")
+        let typesToRead:Set = [HKQuantityType.quantityType(forIdentifier: .heartRate)!,.quantityType(forIdentifier: .activeEnergyBurned)!]
+        healthStore.requestAuthorization(toShare: typesToShare, read: typesToRead) {(success,error) in
+            guard success else {
+                print("Errore alla richiesta di autorizzazioni: \(error!.localizedDescription)")
+                return
+            }
+        }
+        let configuration = HKWorkoutConfiguration()
+        configuration.activityType = .handCycling
+        configuration.locationType = .indoor
+        do {
+            workoutSession = try HKWorkoutSession(healthStore: healthStore, configuration: configuration)
+            workoutBuilder = workoutSession!.associatedWorkoutBuilder()
+            workoutBuilder!.dataSource = HKLiveWorkoutDataSource(healthStore: healthStore, workoutConfiguration: configuration)
+        }
+        catch {
+            print("Errore nella costruzione del workout: \(error)")
+            return
+        }
+        
     }
     func startQueuedUpdates() {
         print("Prima di startDeviceMotionUpdates")
         session = Session()
+        workoutSession!.startActivity(with: Date())
+        workoutBuilder!.beginCollection(withStart: Date(), completion: { (success,error) in
+            guard success else {
+                print("Errore prima di iniziare a prendere dati: \(error!.localizedDescription)")
+                return
+            }
+        })
         motion.startDeviceMotionUpdates(to: queue, withHandler: {
             motion, error in
             if motion != nil {
@@ -37,6 +73,30 @@ class MotionDataManager: ObservableObject {
     }
     
     func stopQueuedUpdates() {
+        workoutSession!.end()
+        workoutBuilder!.endCollection(withEnd: Date(), completion: { (success, error) in
+            guard success else {
+                print("Errore al termine del workoutBuilder: \(error!.localizedDescription)")
+                return
+            }
+            self.workoutBuilder?.finishWorkout(completion: { (workout,error) in
+                guard workout != nil else {
+                    print("Errore prima di finire il workout: \(error!.localizedDescription)")
+                    return
+                }
+                print("Statistics count: \(workout!.allStatistics.count)")
+                workout!.allStatistics.forEach { (key: HKQuantityType, value: HKStatistics) in
+                    print("Key: \(key.description)")
+                    if let quantity = value.maximumQuantity() {
+                           let unit = key.getAssociatedUnit()
+                           let valueInUnit = quantity.doubleValue(for: unit)
+                           print("Type: \(key.identifier), Value: \(valueInUnit) \(unit)")
+                       }
+//                    if let quantity = value.
+                }
+                
+            })
+        })
         if motion.isDeviceMotionActive {
             print("Motion updates are happening. Stopping...")
             motion.stopDeviceMotionUpdates()
