1c15284f7475e8c24893129f72e8628b2fdf64de
Snesnopic <snesnopic@gmail.com>
2024-02-27 12:39:21 +0100

    Testing the Heart rate graph on watchOS

--- CanShaker Watch App/View/LastStatsView.swift
+++ CanShaker Watch App/View/LastStatsView.swift
@@ -13,36 +13,58 @@ import SwiftData
 struct LastStatsView: View {
     @Query var sessions:[Session]
     let connectivity = Connectivity.shared
+    let heartGradient = LinearGradient(
+        gradient: Gradient (
+            colors: [ Color("heartColor").opacity(0.75),
+                      Color("heartColor")
+                .opacity(0.25),
+                      Color.clear ]
+        ),
+        startPoint: .top, endPoint: .bottom)
     var body: some View {
         NavigationStack {
             if !sessions.isEmpty {
-                Chart {
-                    ForEach(sessions.last!.accelData.keys.sorted(),id: \.self) {
-                        time in
-                        BarMark (x:
-    //                            .value("Time", time),
-                            .value("Time", Date(timeIntervalSince1970: time),unit: .second),
-                                 y: .value("Acceleration", (sessions.last!.accelData[time]!)))
-                    }
-                }
-                .chartXAxis{
-                    AxisMarks(values: .automatic(desiredCount: 5)) {
-                        AxisValueLabel(format: Date.FormatStyle().minute(.defaultDigits).second(.defaultDigits)).offset(x: -5)
-                    }
-                }
-                .chartYAxis {
-                    AxisMarks(values: .automatic(desiredCount: 5))
-                }
-                .chartXAxisLabel("Time")
-                .chartYAxisLabel("Speed")
-                .chartLegend(content: {
-                    Text("Acceleration")
-                })
-                .chartForegroundStyleScale([
-                    "Test": .blue
-                ])
-                .navigationTitle("Last session")
-
+                Chart{
+                     ForEach(sessions.last!.heartRateData.keys.sorted(),id: \.self){ time in
+                         
+                        AreaMark (x: .value("Time", Date(timeIntervalSince1970: time)),
+                                   y: .value("BPM", (sessions.last!.heartRateData[time]!)))
+                         
+                         .interpolationMethod(.catmullRom)
+                          .foregroundStyle(heartGradient)
+                          
+                      }
+                     
+                  }
+                  .chartXAxis{
+                      AxisMarks(values: .automatic(desiredCount: 6)) {
+                          AxisValueLabel(format: Date.FormatStyle().minute(.defaultDigits).second(.defaultDigits))
+                      }
+                  }
+                  .chartYAxis {
+                     AxisMarks(position: .trailing) { _ in
+                          AxisValueLabel()
+                      }
+                  }
+                  .chartLegend(content: {
+                      VStack (alignment: .leading){
+                          Text("Range")
+                              .foregroundStyle(.white)
+                          HStack {
+                              Text("\(Int( sessions.last!.heartRateData.values.min()!))-\(Int(sessions.last!.heartRateData.values.max()!))")
+                                  .font(.title2)
+                                  .foregroundStyle(.white)
+                              Text("BPM")
+                                  .foregroundStyle(Color("heartColor"))
+                          }
+                      }
+                  })
+                  .chartForegroundStyleScale([
+                    "Heart Rate": Color("heartColor")
+                  ])
+                  .navigationTitle("Last session")
+                  .padding(.trailing)
+                  
     
             }
             else {
@@ -57,7 +79,7 @@ struct LastStatsView: View {
         }
     }
 }
-
-#Preview {
-    LastStatsView()
-}
+//
+//#Preview {
+//    LastStatsView(sessions: [Session(date: Date(), accelData: [:], duration: 10.0, heartRateData: [0.0:60.0,1.0:70.0,2.0:80.0], calories: 65)])
+//}
