21861fe08385936523be92034ded5609f02e18ea
Snesnopic <snesnopic@gmail.com>
2024-02-27 14:40:34 +0100

    Now before sending the payload, phone and watch exchange their session counts, and watch only sends the necessary data

--- CanShaker Watch App/Support files/Connectivity.swift
+++ CanShaker Watch App/Support files/Connectivity.swift
@@ -18,7 +18,7 @@ final class Connectivity: NSObject, ObservableObject, WCSessionDelegate {
 #endif
     //variabile che iOS userà per accedere alle sessioni
     @Published var sessions:[Session] = []
-    
+    var phoneSessionCount:Int = 0
     static let shared = Connectivity()
     override private init() {
         super.init()
@@ -42,8 +42,21 @@ final class Connectivity: NSObject, ObservableObject, WCSessionDelegate {
         sessions.forEach { session in
             message[UUID().uuidString] = session
         }
-        let data = try! jsonEncoder.encode(sessions)
-        print("Data to send before compression: \(data)")
+        let data:Data!
+        if phoneSessionCount == 0 {
+            data = try! jsonEncoder.encode(sessions)
+        }
+        else {
+            if phoneSessionCount == sessions.count {
+                print("Il telefono ha \(phoneSessionCount), il watch ha \(sessions.count), non devo mandare niente!")
+                return
+            }
+            else {
+                let reducedSessions = sessions.dropLast(sessions.count - phoneSessionCount)
+                data = try! jsonEncoder.encode(Array(reducedSessions))
+            }
+        }
+        print("Data to send before compression: \(data!)")
         do {
             let compressedData = try (data as NSData).compressed(using: .lzma) as Data
             print("Data to send after compression: \(compressedData)")
@@ -69,7 +82,34 @@ final class Connectivity: NSObject, ObservableObject, WCSessionDelegate {
         }
         
     }
-    
+    //ricevi quante sessioni ha il watch e manda quante ne hai tu
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any], replyHandler: @escaping ([String : Any]) -> Void) {
+        print("Received \(message)")
+        
+        let sessionsCount = message["count"] as? Int ?? 0
+        
+        let replyMessage: [String: Any] = ["replyMessage": sessions.count]
+        replyHandler(replyMessage)
+        
+    }
+    //manda il numero di sessioni del watch all'iOS e si aspetta la risposta del suo numero
+    func sendCount() {
+        guard WCSession.default.activationState == .activated else {
+            return
+        }
+        print("Has content pending: \(WCSession.default.hasContentPending.description)")
+#if os(watchOS)
+        print("Is companion app installed\(WCSession.default.isCompanionAppInstalled)")
+#endif
+        let message:[String: Any] = ["count":sessions.count]
+        WCSession.default.sendMessage(message) { reply in
+            self.phoneSessionCount = reply["replyMessage"] as! Int
+        } errorHandler: { error in
+            print("Errore nel mandare il session count ad iOS: \(error)")
+        }
+        
+        
+    }
     func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
         
     }
