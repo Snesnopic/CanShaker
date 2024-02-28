a1c5f871038af1208301a2933a32ce10e9f7bb1b
Snesnopic <snesnopic@gmail.com>
2024-02-28 13:10:38 +0100

    Removed preview to prevent error

--- CanShaker Watch App/View/Achievements/AchievementDetailView.swift
+++ CanShaker Watch App/View/Achievements/AchievementDetailView.swift
@@ -29,7 +29,7 @@ struct AchievementDetailView: View {
         }
     }
 }
-
-#Preview {
-    AchievementDetailView(animation: Namespace().wrappedValue, isPresented: .constant(true), achievement: .constant(Achievement.list.randomElement()))
-}
+//
+//#Preview {
+//    AchievementDetailView(animation: Namespace().wrappedValue, isPresented: .constant(true), achievement: .constant(Achievement.list.randomElement()))
+//}
