9236b5afa43d728f564639a899e9e01e240ca2e0
Giuseppe Casillo <g.casillox2@gmail.com>
2024-02-28 10:44:05 +0100

    Changed the Achievement part (still to finish)

--- CanShaker/View/Achievement/AchievementDetailView.swift
+++ CanShaker/View/Achievement/AchievementDetailView.swift
@@ -8,31 +8,31 @@
 import SwiftUI
 
 struct AchievementDetailView: View {
-    @Binding var achievement: Achievement?
+    @State var achievement: Achievement
     var body: some View {
         
         LinearGradient(gradient: Gradient(colors: [.lightBP, .darkBP]), startPoint: .top, endPoint: .bottom)
             .ignoresSafeArea().overlay {
                 VStack{
-                    if achievement != nil {
+                    
                         Circle()
                             .frame(width: 300, height: 300)
                             .foregroundStyle(.black)
-                        Text(achievement!.title)
+                        Text(achievement.title)
                             .font(.title)
                             .foregroundStyle(.white)
-                        Text(achievement!.subTitle)
+                        Text(achievement.subTitle)
                             .font(.title2)
                             .foregroundStyle(.white)
-                        Text(achievement!.description)
+                        Text(achievement.description)
                             .font(.title3)
                             .foregroundStyle(.white)
-                        Text("Progress: \(achievement!.completion)%")
+                        Text("Progress: \(achievement.completion)%")
                             .font(.headline)
-                        Text("Date: \(achievement!.achievingDate != nil ? "\(achievement!.achievingDate!)" : "N/A")")
+                    Text("Date: \(achievement.achievingDate != nil ? "\(String(describing: achievement.achievingDate))" : "N/A")")
                             .font(.headline)
                             .foregroundStyle(.white)
-                    }
+                    
                 }
                 
                 .foregroundStyle(.white)
