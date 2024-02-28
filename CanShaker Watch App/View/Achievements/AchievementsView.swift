eac24d896bebe73cc07c05d3ecb44c81f5c19351
Snesnopic <snesnopic@gmail.com>
2024-02-28 13:10:04 +0100

    Temporary fix, but now gets the achievements from SwiftData

--- CanShaker Watch App/View/Achievements/AchievementsView.swift
+++ CanShaker Watch App/View/Achievements/AchievementsView.swift
@@ -6,57 +6,65 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct AchievementsView: View {
     @Namespace private var animation
     @State var isPresented:Bool = false
     @State var selectedAchievement:Achievement? = nil
+    @Query var achievements:[Achievement]
+    @Environment(\.modelContext) private var context
     var body: some View {
         NavigationStack {
             if !isPresented {
                 ScrollView {
                     Grid() {
-                        ForEach(1...Achievement.list.count/3, id: \.self){
-                            i in
-                            GridRow {
-                                Button(action: {
-                                    selectedAchievement = Achievement.list[(i-1)*3]
-                                    withAnimation {
-                                        isPresented = true
-                                    }
-                                }, label: {
-                                    ExtractedView(achievement: Achievement.list[(i-1)*3], animation: animation)
-                                        .opacity(selectedAchievement == nil || selectedAchievement!.id == Achievement.list[(i-1)*3].id ? 1.0 : 0.0)
-                                })
-                                .buttonStyle(.plain)
-                                Button(action: {
-                                    selectedAchievement = Achievement.list[(i-1)*3 + 1]
-                                    withAnimation {
-                                        isPresented = true
-                                    }
-                                }, label: {
-                                    
-                                    ExtractedView(achievement: Achievement.list[(i-1)*3+1],animation: animation)
-                                        .opacity(selectedAchievement == nil || selectedAchievement!.id == Achievement.list[(i-1)*3+1].id ? 1.0 : 0.0)
-                                    
-                                })
-                                .buttonStyle(.plain)
-                                Button(action: {
-                                    selectedAchievement = Achievement.list[(i-1)*3+2]
-                                    withAnimation {
-                                        isPresented = true
-                                    }
-                                }, label: {
-                                    
-                                    ExtractedView(achievement: Achievement.list[(i-1)*3+2],animation: animation)
-                                        .opacity(selectedAchievement == nil || selectedAchievement!.id == Achievement.list[(i-1)*3+2].id ? 1.0 : 0.0)
-                                    
-                                })
-                                .buttonStyle(.plain)
+                        if !achievements.isEmpty {
+                            ForEach(1...achievements.count/3, id: \.self){
+                                i in
+                                GridRow {
+                                    Button(action: {
+                                        selectedAchievement = achievements[(i-1)*3]
+                                        withAnimation {
+                                            isPresented = true
+                                        }
+                                    }, label: {
+                                        ExtractedView(achievement: achievements[(i-1)*3], animation: animation)
+                                            .opacity(selectedAchievement == nil || selectedAchievement!.id == achievements[(i-1)*3].id ? 1.0 : 0.0)
+                                    })
+                                    .buttonStyle(.plain)
+                                    Button(action: {
+                                        selectedAchievement = achievements[(i-1)*3 + 1]
+                                        withAnimation {
+                                            isPresented = true
+                                        }
+                                    }, label: {
+                                        
+                                        ExtractedView(achievement: achievements[(i-1)*3+1],animation: animation)
+                                            .opacity(selectedAchievement == nil || selectedAchievement!.id == achievements[(i-1)*3+1].id ? 1.0 : 0.0)
+                                        
+                                    })
+                                    .buttonStyle(.plain)
+                                    Button(action: {
+                                        selectedAchievement = achievements[(i-1)*3+2]
+                                        withAnimation {
+                                            isPresented = true
+                                        }
+                                    }, label: {
+                                        
+                                        ExtractedView(achievement: achievements[(i-1)*3+2],animation: animation)
+                                            .opacity(selectedAchievement == nil || selectedAchievement!.id == achievements[(i-1)*3+2].id ? 1.0 : 0.0)
+                                        
+                                    })
+                                    .buttonStyle(.plain)
+                                }
                             }
                         }
                     }
                     .navigationTitle("Achievements")
+                    .onAppear {
+                        Achievement.ensureAchievementsExist(context: context, achievements: achievements)
+                    }
                 }
             }
             else {
