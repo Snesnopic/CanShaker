71d4a6126b7cc5d16c113a59d80976cb09225289
Snesnopic <snesnopic@gmail.com>
2024-02-28 12:19:03 +0100

    Fixed bug

--- CanShaker Watch App/View/LastStatsView.swift
+++ CanShaker Watch App/View/LastStatsView.swift
@@ -52,7 +52,7 @@ struct LastStatsView: View {
                             Text("Range")
                                 .foregroundStyle(.white)
                             HStack {
-                                Text("\(Int(sessions.last!.heartRateData.values.min()!))-\(Int(sessions.last!.heartRateData.values.min()!))")
+                                Text("\(Int(sessions.last!.heartRateData.values.min()!))-\(Int(sessions.last!.heartRateData.values.max()!))")
                                     .font(.title2)
                                     .foregroundStyle(.white)
                                 Text("BPM")
