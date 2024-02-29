a71b8e2b55009b99c3ff39068f6c184ba7eef4c1
Snesnopic <snesnopic@gmail.com>
2024-02-29 16:21:15 +0100

    Now sessions are partitioned and correctly ordered, we just need to remove hours, minutes and seconds

--- CanShaker/View/Dashboard/ArchiveView.swift
+++ CanShaker/View/Dashboard/ArchiveView.swift
@@ -9,58 +9,61 @@ import SwiftUI
 import SwiftData
 import Charts
 struct ArchiveView: View {
-    @Query private var sessions:[Session]
+    var sessions: [Session]
+    private var partitionedDates: [[Session]] = [[]]
+    init(sessions:[Session]) {
+        self.sessions = sessions
+        let groups = sessions.reduce(into: [[Session]]()) { acc, session in
+            for i in acc.indices {
+                if acc[i][0].date.relativeToNow == session.date.relativeToNow {
+                    acc[i].append(session)
+                    return
+                }
+            }
+            acc.append([session])
+        }
+        partitionedDates = groups
+    }
     var body: some View {
         List{
-            ForEach(dates(), id: \.self) { date in
-                Section(header: Text(String(describing: date.relativeToNow))){
-                    ForEach(sessions){ session in
-                        if(session.date.formatted(.dateTime.day()) == date.formatted(.dateTime.day())){
-                            Text("Sessione durata \(session.duration)")
-                        }
-                        
+            ForEach(partitionedDates.sorted(by: { array1, array2 in
+                array1.first!.date > array2.first!.date
+            }), id: \.self) {
+                partition in
+                Section {
+                    ForEach(partition.sorted(by: { session1, session2 in
+                        session1.date > session2.date
+                    }), id: \.self) {
+                        session in
+                        Text("Durata sessione: \(session.duration)")
                     }
+                } header:
+                {
+                    Text(partition.first!.date.relativeToNow)
                 }
-                
             }
         }
         .listStyle(.plain)
     }
-    
-    
-    
-    
-    func dates() -> [Date] {
-        var dates: Set<Date> = []
-        for session in sessions {
-            if(!dates.contains(session.date)){
-                dates.insert(session.date)
-            }
-            
-        }
-        
-        let orderedDates = Array(dates.sorted(by: {$0 > $1}))
-        return orderedDates
-    }
 }
-
-#Preview {
-    
-    let config = ModelConfiguration(isStoredInMemoryOnly: true)
-    let container = try! ModelContainer(for: Session.self, configurations: config)
-
-    
-    var accelD:[TimeInterval:Double] = [:]
-    var heartRate:[TimeInterval:Double] = [:]
-    for i in 1...10 {
-        accelD[Double(i)*0.3] = Double.random(in: 50...140)
-        heartRate[Double(i)*0.3] = Double.random(in: 50...140)
-    }
-    for i in 0..<3{
-        container.mainContext.insert(Session(date: Date(timeIntervalSinceNow: 0.0) + TimeInterval(i*2000000), accelData: accelD, duration: Double.random(in: (0.0)...(100.0)), heartRateData: heartRate))
-        container.mainContext.insert(Session(date: Date(timeIntervalSince1970: 0.0) + TimeInterval(i*2000000), accelData: accelD, duration: 50.0/3.0, heartRateData: heartRate))
-    }
-    return ArchiveView()
-        .modelContainer(container)
-    
-}
+//
+//#Preview {
+//    
+//    let config = ModelConfiguration(isStoredInMemoryOnly: true)
+//    let container = try! ModelContainer(for: Session.self, configurations: config)
+//
+//    
+//    var accelD:[TimeInterval:Double] = [:]
+//    var heartRate:[TimeInterval:Double] = [:]
+//    for i in 1...10 {
+//        accelD[Double(i)*0.3] = Double.random(in: 50...140)
+//        heartRate[Double(i)*0.3] = Double.random(in: 50...140)
+//    }
+//    for i in 0..<3{
+//        container.mainContext.insert(Session(date: Date(timeIntervalSinceNow: 0.0) + TimeInterval(i*2000000), accelData: accelD, duration: Double.random(in: (0.0)...(100.0)), heartRateData: heartRate))
+//        container.mainContext.insert(Session(date: Date(timeIntervalSince1970: 0.0) + TimeInterval(i*2000000), accelData: accelD, duration: 50.0/3.0, heartRateData: heartRate))
+//    }
+//    return ArchiveView()
+//        .modelContainer(container)
+//    
+//}
