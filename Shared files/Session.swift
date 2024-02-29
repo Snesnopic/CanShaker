f12e0e4718c2bd350548c3c28a050be54ddb88eb
Snesnopic <snesnopic@gmail.com>
2024-02-29 15:43:35 +0100

    Session now has explicit UUID

--- Shared files/Session.swift
+++ Shared files/Session.swift
@@ -10,12 +10,13 @@ import CoreMotion
 import SwiftData
 
 @Model 
-class Session: Codable {
+class Session: Codable, Identifiable {
     enum CodingKeys: CodingKey {
-        case date, accelData, duration, heartRateData, calories
+        case uuid, date, accelData, duration, heartRateData, calories
     }
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(uuid, forKey: .uuid)
         try container.encode(date, forKey: .date)
         try container.encode(accelData, forKey: .accelData)
         try container.encode(duration, forKey: .duration)
@@ -26,12 +27,14 @@ class Session: Codable {
     required public convenience init(from decoder: Decoder) throws {
         self.init()
         let container = try decoder.container(keyedBy: CodingKeys.self)
+        uuid = try container.decode(UUID.self, forKey: .uuid)
         date = try container.decode(Date.self, forKey: .date)
         accelData = try container.decode([TimeInterval:Double].self, forKey: .accelData)
         duration = try container.decode(TimeInterval.self, forKey: .duration)
         heartRateData = try container.decode([TimeInterval:Double].self, forKey: .heartRateData)
         calories = try container.decode(Double.self, forKey: .calories)
     }
+    var uuid: UUID = UUID()
     var date: Date
     var accelData: [TimeInterval:Double]
     var duration: TimeInterval
