8f16a0c8daa49d78e96cc3daeefd1e0d2eb429ff
Dave-Ed-Cast <dcastaldi23@fed.idserve.net>
2024-02-29 10:56:36 +0100

    Commit for now

--- CanShaker/View/Advices/AdvicesView.swift
+++ CanShaker/View/Advices/AdvicesView.swift
@@ -14,16 +14,16 @@ struct AdvicesView: View {
                 //TODO: add list destination with advices category
                 VStack{
                     NavigationLink(destination: AdvicesListView(advCategory: .hygiene), label: {
-                        AdviceButtonStyle(img: "heart.text.square.fill", category: "Hygiene", cDescription: "Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum")
+                        AdviceButtonStyle(img: "heart.text.square.fill", category: "Hygiene", cDescription: "Learn how to keep your can always in top shape")
                     })
                     
                     NavigationLink(destination: AdvicesListView(advCategory: .methods), label: {
-                        AdviceButtonStyle(img: "book.closed.fill", category: "Methods", cDescription: "Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum")
+                        AdviceButtonStyle(img: "book.closed.fill", category: "Methods", cDescription: "There are secret graffiti techniques to explore")
                     })
                     
                     
                     NavigationLink(destination: AdvicesListView(advCategory: .safety), label: {
-                        AdviceButtonStyle(img: "exclamationmark.triangle.fill", category: "Safety", cDescription: "Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum")
+                        AdviceButtonStyle(img: "exclamationmark.triangle.fill", category: "Safety", cDescription: "You have to keep your can safe")
                     })
                 }
                 .navigationTitle("Advices")
--- CanShaker/View/Advices/Components/AdviceButtonStyle.swift
+++ CanShaker/View/Advices/Components/AdviceButtonStyle.swift
@@ -14,19 +14,11 @@ struct AdviceButtonStyle: View {
     var body: some View {
         
         ZStack{
-            RoundedRectangle(cornerRadius: 15)
-                .responsiveFrame(widthPercentage: 95, heightPercentage: 30)
-                .foregroundStyle(Color("boxColor"))
-                .opacity(0.4)
-                .shadow(radius: 20)
+            
             //TODO: This will be the image ^
-            RoundedRectangle(cornerRadius: 15)
-                .responsiveFrame(widthPercentage: 95, heightPercentage: 8)
-                .foregroundStyle(Color("boxColor"))
-                .opacity(0.4)
-                .shadow(radius: 20)
-                .padding(.top, 190)
+            
             VStack(alignment: .leading) {
+                Spacer()
                 HStack{
                     Image(systemName: img)
                         .font(.largeTitle)
@@ -34,17 +26,26 @@ struct AdviceButtonStyle: View {
                         .bold()
                         .font(.title)
                 }
-                .padding(.bottom, 20)
                 Text(cDescription)
+                    .font(.subheadline)
+                    .fontWeight(.medium)
+                ZStack(alignment: .leading) {
+                    UnevenRoundedRectangle(cornerRadii: RectangleCornerRadii(topLeading: 0, bottomLeading: 20, bottomTrailing: 20, topTrailing: 0))
+                        .responsiveFrame(widthPercentage: 95, heightPercentage: 5)
+                        .foregroundColor(Color("boxColor"))
+                        .opacity(0.4)
+                        .shadow(radius: 20)
+
+                    Text("Footer")
+                        .font(.footnote)
+                }
+                
             }
-            .responsiveFrame(widthPercentage: 90, heightPercentage: 30, alignment: .bottomLeading)
-            .padding(.bottom, 20)
-            .foregroundStyle(.white)
         }
         .preferredColorScheme(.dark)
     }
 }
 
 #Preview{
-    AdviceButtonStyle(img: "book.closed.fill", category: "Methods", cDescription: "Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum")
+    AdviceButtonStyle(img: "book.closed.fill", category: "Methods", cDescription: "Lorem ipsum lorem ipsum lorem")
 }
