0af51e6f3a8128497bbe1917daa98fe1230b41aa
Snesnopic <snesnopic@gmail.com>
2024-03-01 16:25:19 +0100

    Added working preview

--- CanShaker/View/Dashboard/DashboardView.swift
+++ CanShaker/View/Dashboard/DashboardView.swift
@@ -31,7 +31,6 @@ struct DashboardView: View {
                         }
                     }
                     FeedbackView()
-                    //TODO: Adjust the view
                     LastSessionView()
                         .padding(.top, 5)
                     Spacer()
@@ -51,5 +50,29 @@ struct DashboardView: View {
 }
 
 #Preview {
-    DashboardView()
+    let config = ModelConfiguration(isStoredInMemoryOnly: true)
+    let container = try! ModelContainer(for: Session.self,Achievement.self, configurations: config)
+    
+    let list = [
+        Achievement(title: "Heroic Shaker", subTitle: "Master of Motion", desc: "You've shaken your can over 1000 times! Keep it up!", completion: 0, isAchieved: false, imageName: "splashBlue"),
+        Achievement(title: "Steady Rhythm", subTitle: "No Moment of Pause", desc: "You've maintained a steady average speed for over 30 minutes!", completion: 0, isAchieved: false, imageName: "flameRed"),
+        Achievement(title: "Race Against Time", subTitle: "Fast as the Wind", desc: "You've reached an average speed of over 15 km/h!", completion: 0, isAchieved: false, imageName: "flameRed"),
+        Achievement(title: "Calm Heartbeat", subTitle: "State of Relaxation", desc: "You've maintained a heartbeat below 100 bpm throughout the entire session!", completion: 0, isAchieved: false, imageName: "splashBlue"),
+        Achievement(title: "Persistence", subTitle: "Never Give Up!", desc: "You've used the app for more than 7 consecutive days!", completion: 7, isAchieved: false, imageName: "splashBlue"),
+        Achievement(title: "Master of Energy", subTitle: "Heartbeats, Calories, Speed!", desc: "You've achieved a perfect score on all metrics: heartbeats, burned calories, and speed!", completion: 0, isAchieved: false, imageName: "flameRed")
+    ]
+    list.forEach { achievement in
+        container.mainContext.insert(achievement)
+    }
+    
+    var accelD:[TimeInterval:Double] = [:]
+    var heartRate:[TimeInterval:Double] = [:]
+    for i in 1...10 {
+        accelD[Double(i)*0.3] = Double.random(in: 50...140)
+        heartRate[Double(i)*0.3] = Double.random(in: 50...140)
+    }
+    
+    container.mainContext.insert(Session(date: Date(), accelData: accelD, duration: 50.0/3.0, heartRateData: heartRate))
+    return DashboardView()
+        .modelContainer(container)
 }
