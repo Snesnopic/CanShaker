1eb2bc0a2c143bb544ecaf7bc119a65e43277c87
Snesnopic <snesnopic@gmail.com>
2024-03-01 15:20:14 +0100

    Merge fix

--- CanShaker/View/Dashboard/History/HistoryView.swift
+++ CanShaker/View/Dashboard/History/HistoryView.swift
@@ -9,23 +9,38 @@ import SwiftUI
 import SwiftData
 import Charts
 struct HistoryView: View {
+    func containAnyTimeWord(_ string1: String, _ string2: String) -> Bool {
+        let timeWords = ["hour", "minute", "second"]
+        
+        let containsWord1 = timeWords.contains { word in
+            string1.lowercased().contains(word)
+        }
+        
+        let containsWord2 = timeWords.contains { word in
+            string2.lowercased().contains(word)
+        }
+        
+        return containsWord1 && containsWord2
+    }
+    
     var sessions: [Session]
     private var partitionedDates: [[Session]] = [[]]
     init(sessions:[Session]) {
         self.sessions = sessions
-        let groups = sessions.reduce(into: [[Session]]()) { acc, session in
+        partitionedDates = sessions.reduce(into: [[Session]]()) { acc, session in
             for i in acc.indices {
-                if acc[i][0].date.relativeToNow == session.date.relativeToNow {
+                if acc[i][0].date.relativeToNow == session.date.relativeToNow ||
+                   containAnyTimeWord(acc[i][0].date.relativeToNow, session.date.relativeToNow)
+                {
                     acc[i].append(session)
                     return
                 }
             }
             acc.append([session])
         }
-        partitionedDates = groups
+
     }
     var body: some View {
-        
         List{
             ForEach(partitionedDates.sorted(by: { array1, array2 in
                 array1.first!.date > array2.first!.date
@@ -40,35 +55,42 @@ struct HistoryView: View {
                     }
                 } header:
                 {
-                    Text(partition.first!.date.relativeToNow)
+                    if partition.first!.date.isToday {
+                        Text("Today")
+                    }
+                    else {
+                        Text(partition.first!.date.relativeToNow)
+                    }
                 }
             }
         }
+        .navigationTitle("History")
         .listStyle(.plain)
-        .preferredColorScheme(.dark)
     }
-    
 }
 
 #Preview {
     
     let config = ModelConfiguration(isStoredInMemoryOnly: true)
     let container = try! ModelContainer(for: Session.self, configurations: config)
+
     
-    var sessioni: [Session] = []
     var accelD:[TimeInterval:Double] = [:]
     var heartRate:[TimeInterval:Double] = [:]
     for i in 1...10 {
         accelD[Double(i)*0.3] = Double.random(in: 50...140)
         heartRate[Double(i)*0.3] = Double.random(in: 50...140)
     }
-    for i in 0..<3{
-        container.mainContext.insert(Session(date: Date(timeIntervalSinceNow: 0.0) + TimeInterval(i*2000000), accelData: accelD, duration: Double.random(in: (0.0)...(100.0)), heartRateData: heartRate))
-        container.mainContext.insert(Session(date: Date(timeIntervalSince1970: 0.0) + TimeInterval(i*2000000), accelData: accelD, duration: 50.0/3.0, heartRateData: heartRate))
-        sessioni.append(Session(date: Date(timeIntervalSinceNow: 0.0) + TimeInterval(i*2000000), accelData: accelD, duration: Double.random(in: (0.0)...(100.0)), heartRateData: heartRate))
+    for i in 0..<100{
+        container.mainContext.insert(Session(date: Date(timeIntervalSinceNow: 0.0) - TimeInterval(Double(i)*Double.random(in: (0.0)...(10000.0))), accelData: accelD, duration: Double.random(in: 30.0...6000.0), heartRateData: heartRate))
+        container.mainContext.insert(Session(date: Date(timeIntervalSinceNow: 0.0) - TimeInterval(Double(i)*Double.random(in: (0.0)...(10000.0))), accelData: accelD, duration: Double.random(in: 30.0...6000.0), heartRateData: heartRate))
     }
-    
-    return HistoryView(sessions: sessioni)
-        .modelContainer(container)
+    do {
+        return try HistoryView(sessions: container.mainContext.fetch(FetchDescriptor<Session>()))
+            .modelContainer(container)
+    }
+    catch {
+        return Text("Preview error")
+    }
     
 }
