500d571ed738bb60b70fa38abd91341cc7c9227e
Giuseppe Casillo <g.casillox2@gmail.com>
2024-03-01 09:36:56 +0100

    Changed Archive to History for clarity

--- CanShaker/View/Dashboard/HistoryElementStyle.swift
+++ CanShaker/View/Dashboard/HistoryElementStyle.swift
@@ -1,5 +1,5 @@
 //
-//  ArchiveElementStyle.swift
+//  HistoryElementStyle.swift
 //  CanShaker
 //
 //  Created by Davide Castaldi on 29/02/24.
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct ArchiveElementStyleView: View {
+struct HistoryElementStyle: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    ArchiveElementStyleView()
+    HistoryElementStyle()
 }
