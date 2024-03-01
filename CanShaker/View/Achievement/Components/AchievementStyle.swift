4eb2efa4eb009b343824f129ba50eba8a7f35066
Snesnopic <snesnopic@gmail.com>
2024-03-01 11:14:41 +0100

    Preview adjusted

--- CanShaker/View/Achievement/Components/AchievementStyle.swift
+++ CanShaker/View/Achievement/Components/AchievementStyle.swift
@@ -23,5 +23,12 @@ struct AchievementStyle: View {
 }
 
 #Preview {
-    AchievementStyle(title: "Title")
+    let config = ModelConfiguration(isStoredInMemoryOnly: true)
+    let container = try! ModelContainer(for: Achievement.self, configurations: config)
+
+    return AchievementStyle(achievement: Achievement(title: "prova", subTitle: "prova", desc: "prova", completion: 53, achievingDate: nil, isAchieved: false, imageName: "splashBlue"))
+        .modelContainer(container)
+        .background {
+            Color.black
+        }
 }
