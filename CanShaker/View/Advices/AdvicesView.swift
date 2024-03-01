9e4f949e9d99aba098b7e2df2003951e65c71d5e
Giuseppe Casillo <g.casillox2@gmail.com>
2024-03-01 11:25:47 +0100

    Fixed color problem in adviceView

--- CanShaker/View/Advices/AdvicesView.swift
+++ CanShaker/View/Advices/AdvicesView.swift
@@ -14,12 +14,15 @@ struct AdvicesView: View {
             VStack(spacing: 15){
                     NavigationLink(destination: AdvicesListView(advCategory: .hygiene), label: {
                         AdviceButtonStyle(img: "heart.text.square.fill", category: "Hygiene", cDescription: "Learn how to keep your can always in top shape", backgroundImg: "Soap")
+                            .accentColor(.white)
                     })
                     NavigationLink(destination: AdvicesListView(advCategory: .methods), label: {
                         AdviceButtonStyle(img: "book.closed.fill", category: "Methods", cDescription: "There are secret graffiti techniques to explore", backgroundImg: "Soap")
+                            .accentColor(.white)
                     })
                     NavigationLink(destination: AdvicesListView(advCategory: .safety), label: {
                         AdviceButtonStyle(img: "exclamationmark.triangle.fill", category: "Safety", cDescription: "You have to keep your can safe", backgroundImg: "Soap")
+                            .accentColor(.white)
                     })
                 Spacer()
                 }
