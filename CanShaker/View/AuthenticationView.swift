90615bb73cfb061417eb58d3c06cf740be0ca837
Giuseppe Casillo <g.casillox2@gmail.com>
2024-03-04 09:58:13 +0100

    Added Authentication for the app

--- CanShaker/View/AuthenticationView.swift
+++ CanShaker/View/AuthenticationView.swift
@@ -6,35 +6,69 @@
 //
 
 import SwiftUI
+import LocalAuthentication
 
 struct AuthenticationView: View {
     @State private var isUnlocked = false
+    @State private var test = "just in"
     var body: some View {
         VStack{
+            Text(test)
             Button(action: {
-                faceIdAuthentication()
+                authenticateUser()
             }, label: {
-               
-                    RoundedRectangle(cornerRadius: 20)
-                        .responsiveFrame(widthPercentage: 70, heightPercentage: 7)
-                        .foregroundStyle(Color("boxColor"))
-                        .opacity(0.5)
-                        .overlay{
-                            HStack{
-                                Image(systemName: "faceid")
-                                    .font(.largeTitle)
-                                Text("Authenticate with face ID")
-                                    .foregroundStyle(.white)
-                            }
-                        }
-                    
-                    
+                
+                RoundedRectangle(cornerRadius: 20)
+                    .responsiveFrame(widthPercentage: 70, heightPercentage: 7)
+                    .foregroundStyle(Color("boxColor"))
+                    .opacity(0.5)
+                    .overlay{
+                            Text("Authenticate")
+                                .foregroundStyle(.white)
+                        
+                    }
+                
+                
             })
         }
         .preferredColorScheme(.dark)
     }
     
-    
+    func authenticateUser() {
+        let authenticationContext = LAContext()
+        var error: NSError?
+        
+        // Check if the device can evaluate the policy.
+        if authenticationContext.canEvaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            
+            authenticationContext.evaluatePolicy( .deviceOwnerAuthenticationWithBiometrics, localizedReason: "Use youre face to unlock.", reply: { (success, evalPolicyError) in
+                
+                if success {
+                    test = "good with faceID"
+                } else {
+                    test = "failed face ID"
+                }
+                
+            })
+            
+        }else {
+            
+            test = "no auth found"
+//            if authenticationContext.canEvaluatePolicy(LAPolicy.deviceOwnerAuthentication, error: &error) {
+//                
+//                authenticationContext.evaluatePolicy( .deviceOwnerAuthentication, localizedReason: "Touch the Touch ID sensor to unlock.", reply: { (success, evalPolicyError) in
+//                    
+//                    if success {
+//                        test = "good with passcode"
+//                    } else {
+//                        test = "failed passcode"
+//                    }
+//                    
+//                })
+//                
+//            }
+        }
+    }
 }
 
 
