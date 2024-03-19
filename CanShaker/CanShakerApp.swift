3fc3ca2b4418f1f5aaec079a43cf2577cfc08139
Snesnopic <snesnopic@gmail.com>
2024-03-19 13:05:10 +0100

    Now elements of the tabBar are unfilled when not selected

--- CanShaker/CanShakerApp.swift
+++ CanShaker/CanShakerApp.swift
@@ -19,15 +19,26 @@ struct CanShakerApp: App {
         WindowGroup {
             TabView(selection: $tabBarSelection) {
                 DashboardView().tabItem {
-                    Label("Dashboard", systemImage: "chart.bar.fill")
+                    Label("Dashboard", systemImage: "chart.bar")
+                        .if(tabBarSelection != 0, transform: { view in
+                            view.environment(\.symbolVariants, .none)
+                        })
+                    
                 }
                 .tag(0)
                 AdvicesView().tabItem {
                     Label("Advice", systemImage: "list.bullet.rectangle")
+                        .if(tabBarSelection != 1, transform: { view in
+                            view.environment(\.symbolVariants, .none)
+                        })
+                    
                 }
                 .tag(1)
                 AchievementsView().tabItem {
                     Label("Achievements", systemImage: "trophy")
+                        .if(tabBarSelection != 2, transform: { view in
+                            view.environment(\.symbolVariants, .none)
+                        })
                 }
                 .tag(2)
             }
