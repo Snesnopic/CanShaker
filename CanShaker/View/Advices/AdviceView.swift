9c61a80aa0f14eb24c40fb9aedfec2251dc2c683
Dave-Ed-Cast <dcastaldi23@fed.idserve.net>
2024-03-20 13:58:25 +0100

    Canged the position of padding for readability

--- CanShaker/View/Advices/AdviceView.swift
+++ CanShaker/View/Advices/AdviceView.swift
@@ -11,29 +11,30 @@ struct AdviceView: View {
     var body: some View {
         NavigationStack {
             
-                VStack(spacing: 15){
-                    NavigationLink(destination: AdviceListView(advCategory: .hygiene), label: {
-                        AdviceButtonStyle(img: "heart.text.square.fill", category: String(localized: "Hygiene"), cDescription: String(localized: "Learn how to keep your can always in top shape"), backgroundImg: "hygiene")
-                        
-                    })
-                    .padding(.top, 20)
-                    NavigationLink(destination: AdviceListView(advCategory: .methods), label: {
-                        AdviceButtonStyle(img: "book.closed.fill", category: String(localized: "Methods"), cDescription: String(localized: "There are secret graffiti techniques to explore"), backgroundImg: "methods")
-                        
-                    })
-                    NavigationLink(destination: AdviceListView(advCategory: .safety), label: {
-                        AdviceButtonStyle(img: "exclamationmark.triangle.fill", category: String(localized: "Safety"), cDescription: String(localized: "You have to keep your can safe"), backgroundImg: "safety")
-                        
-                    })
-                    Spacer()
-                }
-                .accentColor(.white)
-                .navigationTitle(String(localized: "Advice"))
-                .preferredColorScheme(.dark)
-                .frame(maxWidth: .infinity)
-                .background{
-                    Color.bgGray
-                }
+            VStack(spacing: 15) {
+                NavigationLink(destination: AdviceListView(advCategory: .hygiene), label: {
+                    AdviceButtonStyle(img: "heart.text.square.fill", category: String(localized: "Hygiene"), cDescription: String(localized: "Learn how to keep your can always in top shape"), backgroundImg: "hygiene")
+                    
+                })
+                
+                NavigationLink(destination: AdviceListView(advCategory: .methods), label: {
+                    AdviceButtonStyle(img: "book.closed.fill", category: String(localized: "Methods"), cDescription: String(localized: "There are secret graffiti techniques to explore"), backgroundImg: "methods")
+                    
+                })
+                NavigationLink(destination: AdviceListView(advCategory: .safety), label: {
+                    AdviceButtonStyle(img: "exclamationmark.triangle.fill", category: String(localized: "Safety"), cDescription: String(localized: "You have to keep your can safe"), backgroundImg: "safety")
+                    
+                })
+                Spacer()
+            }
+            .padding(.top, 20)
+            .accentColor(.white)
+            .navigationTitle(String(localized: "Advice"))
+            .preferredColorScheme(.dark)
+            .frame(maxWidth: .infinity)
+            .background{
+                Color.bgGray
+            }
         }
     }
 }
