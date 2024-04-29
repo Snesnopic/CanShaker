8ba20d26bf6f6279c02e01f4dd46784433599353
Snesnopic <snesnopic@gmail.com>
2024-04-29 16:15:33 +0200

    Whitespace changes

--- CanShaker/View/Achievement/Components/AchievementStyle.swift
+++ CanShaker/View/Achievement/Components/AchievementStyle.swift
@@ -12,15 +12,15 @@ struct AchievementStyle: View {
     @State var achievement: Achievement
     var body: some View {
         VStack{
-                Circle()
-                    .responsiveFrame(widthPercentage: 20, aspectRatio: (1,1))
-                    .foregroundStyle(Color.boxDetail)
-                    .overlay {
-                        Image(achievement.imageName)
-                            .resizable()
-                            .padding(.all)
-                    }
-                
+            Circle()
+                .responsiveFrame(widthPercentage: 20, aspectRatio: (1,1))
+                .foregroundStyle(Color.boxDetail)
+                .overlay {
+                    Image(achievement.imageName)
+                        .resizable()
+                        .padding(.all)
+                }
+            
             Text(achievement.title)
                 .foregroundStyle(.white)
                 .font(.callout)
@@ -34,7 +34,6 @@ struct AchievementStyle: View {
 #Preview {
     let config = ModelConfiguration(isStoredInMemoryOnly: true)
     let container = try! ModelContainer(for: Achievement.self, configurations: config)
-
     return AchievementStyle(achievement: Achievement(title: "prova", subTitle: "prova", desc: "prova", completion: 53, achievingDate: nil, isAchieved: false, imageName: "splashBlue"))
         .modelContainer(container)
         .background {
