89bf0bf7082a4d9687cbb58f016d5b570ed2449c
Snesnopic <snesnopic@gmail.com>
2024-05-08 15:15:18 +0200

    Diversified achievement images a bit

--- Shared files/Achievement.swift
+++ Shared files/Achievement.swift
@@ -39,12 +39,12 @@ class Achievement: Identifiable{
         if achievements.isEmpty {
             do{
                 let list = [
-                    Achievement(title: String(localized: "Heroic Shaker"), subTitle: String(localized: "Master of Motion"), desc: String(localized: "You've shaken your can over 1000 times! Keep it up!"), completion: 0, isAchieved: false, imageName: "splashBlue"),
+                    Achievement(title: String(localized: "Heroic Shaker"), subTitle: String(localized: "Master of Motion"), desc: String(localized: "You've shaken your can over 1000 times! Keep it up!"), completion: 0, isAchieved: false, imageName: "emptyCan"),
                     Achievement(title: String(localized: "Steady Rhythm"), subTitle: String(localized: "No Moment of Pause"), desc: String(localized: "You've maintained a steady average speed for over 30 minutes!"), completion: 0, isAchieved: false, imageName: "flameRed"),
                     Achievement(title: String(localized: "Race Against Time"), subTitle: String(localized: "Fast as the Wind"), desc: String(localized: "You've reached an average speed of over 15 km/h!"), completion: 0, isAchieved: false, imageName: "flameRed"),
                     Achievement(title: String(localized: "Calm Heartbeat"), subTitle: String(localized: "State of Relaxation"), desc: String(localized: "You've maintained a heartbeat below 100 bpm throughout the entire session!"), completion: 0, isAchieved: false, imageName: "splashBlue"),
                     Achievement(title: String(localized: "Persistence"), subTitle: String(localized: "Never Give Up!"), desc: String(localized: "You've used the app for more than 7 consecutive days!"), completion: 7, isAchieved: false, imageName: "splashBlue"),
-                    Achievement(title: String(localized: "Master of Energy"), subTitle: String(localized: "Heartbeats, Calories, Speed!"), desc: String(localized: "You've achieved a perfect score on all metrics: heartbeats, burned calories, and speed!"), completion: 0, isAchieved: false, imageName: "flameRed")
+                    Achievement(title: String(localized: "Master of Energy"), subTitle: String(localized: "Heartbeats, Calories, Speed!"), desc: String(localized: "You've achieved a perfect score on all metrics: heartbeats, burned calories, and speed!"), completion: 0, isAchieved: false, imageName: "1st")
                 ]
                 list.forEach { achievement in
                     context.insert(achievement)
